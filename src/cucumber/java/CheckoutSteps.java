1faa3a035b7a907b4fca6206b3da7594c1ab7aa9
Ricky Hussmann <ricky.hussmann@gmail.com>
2017-02-13 13:16:04 -0500

    Extending scenaiors to support multiple additions and apples
    * Added a scenario for adding a banana twice
    * Added a scenario for adding an apple

--- src/cucumber/java/CheckoutSteps.java
+++ src/cucumber/java/CheckoutSteps.java
@@ -5,23 +5,25 @@ import static org.junit.Assert.*;
 import com.rhussmann.checkout.Checkout;
 import cucumber.api.java.en.*;
 import cucumber.api.PendingException;
+import java.util.HashMap;
 
 public class CheckoutSteps {
     int bananaPrice = 0;
-    Checkout checkout;
+    Checkout checkout = new Checkout();
+    HashMap<String, Integer> priceMap = new HashMap<String, Integer>();
 
     @Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
     public void the_price_of_a_is_c(String name, int price) throws Throwable {
-	bananaPrice = price;
+	priceMap.put(name, price);
     }
 
     @When("^I checkout (\\d+) \"([^\"]*)\"$")
     public void i_checkout(int itemCount, String itemName) throws Throwable {
-	checkout = new Checkout();
-	checkout.add(itemCount, bananaPrice);
+	int itemPrice = priceMap.get(itemName);
+	checkout.add(itemCount, itemPrice);
     }
 
-    @Then("^the toal price should be (\\d+)c$")
+    @Then("^the total price should be (\\d+)c$")
     public void the_toal_price_should_be_c(int total) throws Throwable {
 	assertEquals(total, checkout.total());
     }
